be040dcfb21b320221a1276c665fb09db525bd48
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-01 09:51:55 -0500

    reduce

--- star-wars/src/index.js
+++ star-wars/src/index.js
@@ -2,6 +2,15 @@ import React from 'React';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,
+  composeEnhancers(applyMiddleware(thunk),
+));
 
 const root = document.getElementById('app-container');
 
