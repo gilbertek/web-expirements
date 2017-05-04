904a33a6b391f71ff753b83cf6a0cb99fccdaac1
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-04 12:28:32 -0500

    about page

---
+++ redux-demo/src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router';
+
+const AboutPage = () =>
+  <div>
+    <h1>About Us</h1>
+    <p>React, Redux and React router for ultra-responsive apps.</p>
+  </div>
+
+export default AboutPage;
