9b99d2c8cfb653da8b518708d6a581919b2d6fd5
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-04 12:29:51 -0500

    add home page

---
+++ redux-demo/src/components/Home/HomePage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className='jumbotron'>
+        <h1>Admin Backend</h1>
+        <p>React, Redux and React router for ultra-responsive apps.</p>
+        <a href='/about' className='btn btn-primary btn-lg'>Learn more</a>
+      </div>
+    );
+  }
+}
+
+// const HomePage = () =>
+//   <div className='jumbotron'>
+//     <h1>Admin Backend</h1>
+//     <p>React, Redux and React router for ultra-responsive apps.</p>
+//     <a href='#about' className='btn btn-primary btn-lg'>Learn more</a>
+//   </div>
+
+export default HomePage;
