0df7571e2abba99a02b498cddc0192162e978b2a
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-10 23:02:37 -0400

    moar

---
+++ login-redux/src/index.js
@@ -0,0 +1,35 @@
+import React from 'React';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,
+  composeEnhancers(applyMiddleware(thunk),
+));
+
+store.dispatch(() => {});
+
+const root = document.getElementById('app-container');
+
+const render = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    root
+  )
+}
+
+render(App);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
---
+++ login-redux/webpack.config.js
@@ -0,0 +1,45 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: resolve(__dirname, 'src'),
+  devtool: 'inline-source-map',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'src'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [ 'babel-loader', ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [ 'style-loader', 'css-loader?modules' ]
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/',
+    contentBase: resolve(__dirname, 'src')
+  },
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'src', 'index.html')
+    })
+  ],
+};
