5f414863039d859a43bb68842afabf6c1fde57fe
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-13 14:22:54 -0400

    reorgs

--- login-redux/src/reducer/login/index.js
+++ login-redux/src/reducer/login/index.js
@@ -1,45 +1,8 @@
-const SET_LOGIN_PENDING = 'SET_LOGIN_PENDING';
-const SET_LOGIN_SUCCESS = 'SET_LOGIN_SUCCESS';
-const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
-
-// Actions
-const setLoginPending = (isLoginPending) => ({
-    type: SET_LOGIN_PENDING, isLoginPending });
-
-const setLoginSuccess = (isLoginSuccess) => ({
-    type: SET_LOGIN_SUCCESS, isLoginSuccess });
-
-const setLoginError = (loginError) => ({
-    type: SET_LOGIN_ERROR, loginError });
-
-
-export function login(email, password) {
-  return dispatch => {
-    dispatch(setLoginPending(true));
-    dispatch(setLoginSuccess(false));
-    dispatch(setLoginError(null));
-
-    callLoginApi(email, password, error => {
-      dispatch(setLoginPending(false));
-
-      if (!error) {
-        dispatch(setLoginSuccess(true));
-      } else {
-        dispatch(setLoginError(error));
-      }
-    });
-  };
-}
-
-function callLoginApi(email, password, callback) {
-  setTimeout(() => {
-    if (email === 'admin@example.com' && password === 'admin') {
-      return callback(null);
-    } else {
-      return callback(new Error('Invalid email and password'));
-    }
-  }, 1000);
-}
+import {
+  SET_LOGIN_PENDING,
+  SET_LOGIN_SUCCESS,
+  SET_LOGIN_ERROR
+} from './constants';
 
 const initialState = {
   isLoginSuccess: false,
