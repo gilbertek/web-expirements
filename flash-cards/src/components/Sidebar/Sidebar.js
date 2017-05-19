741860695327de95e971dc3661b1c357fea1a3a4
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-18 21:53:10 -0400

    moar

--- flash-cards/src/components/Sidebar/Sidebar.js
+++ flash-cards/src/components/Sidebar/Sidebar.js
@@ -10,7 +10,18 @@ class Sidebar extends Component {
     super(props);
 
     this.state = {};
+
+    this.createDeck = this.createDeck.bind(this);
   }
+
+  createDeck = (evt) => {
+    if (evt.which !== 13) return;
+
+    const name = ReactDom.findDomNode(this.ref.add).value;
+    this.props.addDeck(name);
+    this.props.hideDeck();
+  }
+
 
   render() {
     const props = this.props;
@@ -23,7 +34,7 @@ class Sidebar extends Component {
           <li key={i}>{deck.name}</li>
         )}
 
-        { props.addingDeck && <input ref='add' /> }
+        { props.addingDeck && <input ref='add' onKeyPress={this.createDeck} /> }
       </div>
     );
   }
