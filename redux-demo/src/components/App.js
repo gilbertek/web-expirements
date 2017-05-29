2f3f9e0559f71a297e4f82501b8c546133a8743b
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-29 14:24:16 -0400

    no head

--- redux-demo/src/components/App.js
+++ redux-demo/src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'React';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import propTypes from 'prop-types';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
+import Header from './common/Header';
 
 class App extends Component {
   render() {
@@ -11,6 +16,7 @@ class App extends Component {
         <div className='row'>
           <Router>
             <Switch>
+              {/* <Header /> */}
               <Route exact path='/' component={HomePage} />
 
               <Route path='/about' component={AboutPage} />
--- redux-demo/src/components/common/Header.js
+++ redux-demo/src/components/common/Header.js
@@ -1,11 +1,13 @@
 import React from 'React';
 import { NavLink } from 'react-router-dom';
 
-const Header = () =>
-  <nav>
-    <NavLink exact to='/' activeClassName='active'>Home</NavLink>
-    { ' | ' }
-    <NavLink to='/about' activeClassName='active'>About</NavLink>
-  </nav>
+const Header = () => (
+    <nav>
+      <NavLink exact to='/' activeClassName='active'>Home</NavLink>
+      { ' | ' }
+      <NavLink to='/about' activeClassName='active'>About</NavLink>
+    </nav>
+);
+
 
 export default Header;
