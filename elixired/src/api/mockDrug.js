257ab88657757a92fd84e668ab50c3542e63f6a8
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-06-27 20:10:05 -0400

    Drug api

---
+++ elixired/src/api/mockDrug.js
@@ -0,0 +1,605 @@
+const drugs = {
+  "Drugs": [{
+    "DrugName": "Tylenol",
+    "BrandCode": "6732",
+    "DrugID": "d00049",
+    "SampleNDC": "00045018618",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045018618",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol 8 Hour Caplet",
+    "BrandCode": "69730",
+    "DrugID": "d00049",
+    "SampleNDC": "50580029710",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580029710",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol 8 Hour Geltab",
+    "BrandCode": "69740",
+    "DrugID": "d00049",
+    "SampleNDC": "50580050520",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580050520",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol 8 HR Arthritis Pain",
+    "BrandCode": "235616",
+    "DrugID": "d00049",
+    "SampleNDC": "50580078310",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580078310",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Complete NightTime",
+    "BrandCode": "66933",
+    "DrugID": "d04168",
+    "SampleNDC": "50580044322",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580044322",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Multi-Symptom",
+    "BrandCode": "81545",
+    "DrugID": "d03331",
+    "SampleNDC": "50580027324",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580027324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Multi-Symptom Nighttime",
+    "BrandCode": "188115",
+    "DrugID": "d05654",
+    "SampleNDC": "50580028324",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580028324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Sinus Caplet",
+    "BrandCode": "10759",
+    "DrugID": "d03320",
+    "SampleNDC": "00045046124",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045046124",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Sinus Gelcap",
+    "BrandCode": "43742",
+    "DrugID": "d03320",
+    "SampleNDC": "50580038520",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580038520",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Sinus Geltab",
+    "BrandCode": "10766",
+    "DrugID": "d03320",
+    "SampleNDC": "00045050424",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045050424",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Allergy Sinus NightTime",
+    "BrandCode": "11097",
+    "DrugID": "d04168",
+    "SampleNDC": "00045044324",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045044324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Arthritis Caplet",
+    "BrandCode": "90132",
+    "DrugID": "d00049",
+    "SampleNDC": "50580011202",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580011202",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Arthritis Extended Release",
+    "BrandCode": "40891",
+    "DrugID": "d00049",
+    "SampleNDC": "00045029240",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045029240",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Arthritis Gelcap",
+    "BrandCode": "185379",
+    "DrugID": "d00049",
+    "SampleNDC": "50580050820",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580050820",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Arthritis Geltab",
+    "BrandCode": "69728",
+    "DrugID": "d00049",
+    "SampleNDC": "50580029220",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580029220",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Caplet",
+    "BrandCode": "9659",
+    "DrugID": "d00049",
+    "SampleNDC": "00045050130",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045050130",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Caplet Extra Strength",
+    "BrandCode": "8541",
+    "DrugID": "d00049",
+    "SampleNDC": "00045044960",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045044960",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Chest Congestion",
+    "BrandCode": "72765",
+    "DrugID": "d05661",
+    "SampleNDC": "50580025924",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580025924",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Children's Multi-Symptom Cold Plus",
+    "BrandCode": "121768",
+    "DrugID": "d05757",
+    "SampleNDC": "50580039104",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580039104",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Children's Plus Cold",
+    "BrandCode": "109788",
+    "DrugID": "d03331",
+    "SampleNDC": "50580038704",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580038704",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Children's Plus Cold & Cough",
+    "BrandCode": "175579",
+    "DrugID": "d05583",
+    "SampleNDC": "50580025404",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580025404",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Childrens",
+    "BrandCode": "69738",
+    "DrugID": "d00049",
+    "SampleNDC": "50580012301",
+    "Category": "miscellaneous analgesics",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580012301",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Childrens Plus Cough & Runny Nose",
+    "BrandCode": "81703",
+    "DrugID": "d05383",
+    "SampleNDC": "50580024904",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580024904",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Childrens Plus Cough & Sore Throat",
+    "BrandCode": "81700",
+    "DrugID": "d03378",
+    "SampleNDC": "50580247049",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580247049",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold & Cough Nighttime Cool Burst",
+    "BrandCode": "177706",
+    "DrugID": "d05366",
+    "SampleNDC": "50580055908",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580055908",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold & Flu Severe",
+    "BrandCode": "185884",
+    "DrugID": "d05778",
+    "SampleNDC": "50580040225",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580040225",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold & Flu Severe Day Time",
+    "BrandCode": "60107",
+    "DrugID": "d03342",
+    "SampleNDC": "50580028808",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580028808",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold and Flu No Drowsiness Powder",
+    "BrandCode": "5435",
+    "DrugID": "d03342",
+    "SampleNDC": "00045013506",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045013506",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold and Flu Powder",
+    "BrandCode": "5682",
+    "DrugID": "d03347",
+    "SampleNDC": "00045047606",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045047606",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Complete Formula",
+    "BrandCode": "43722",
+    "DrugID": "d03347",
+    "SampleNDC": "50580028524",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580028524",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Day/Night Convenience Pack",
+    "BrandCode": "70368",
+    "DrugID": "d03347",
+    "SampleNDC": "50580007324",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580007324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Head Congestion Day and Night",
+    "BrandCode": "109753",
+    "DrugID": "d05757",
+    "SampleNDC": "50580036920",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580036920",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Head Congestion Nighttime Cool Burst",
+    "BrandCode": "87909",
+    "DrugID": "d05757",
+    "SampleNDC": "50580278241",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580278241",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Head Congestion Severe",
+    "BrandCode": "204198",
+    "DrugID": "d05544",
+    "SampleNDC": "50580056625",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580056625",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Head Congestion Severe (obsolete)",
+    "BrandCode": "81695",
+    "DrugID": "d05778",
+    "SampleNDC": "50580026150",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580026150",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Infants",
+    "BrandCode": "9329",
+    "DrugID": "d03289",
+    "SampleNDC": "00045050315",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045050315",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Max Day",
+    "BrandCode": "235117",
+    "DrugID": "d05583",
+    "SampleNDC": "50580051501",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580051501",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Daytime",
+    "BrandCode": "90297",
+    "DrugID": "d05583",
+    "SampleNDC": "50580025708",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580025708",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Nighttime",
+    "BrandCode": "89204",
+    "DrugID": "d04793",
+    "SampleNDC": "50580026908",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580026908",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Nighttime Cool Burst",
+    "BrandCode": "87907",
+    "DrugID": "d05757",
+    "SampleNDC": "50580026324",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580026324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Nighttime Rapid Release",
+    "BrandCode": "169613",
+    "DrugID": "d05757",
+    "SampleNDC": "50580039524",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580039524",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Rapid Release Day Night",
+    "BrandCode": "172241",
+    "DrugID": "d05757",
+    "SampleNDC": "50580028220",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580028220",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Severe",
+    "BrandCode": "89131",
+    "DrugID": "d05778",
+    "SampleNDC": "50580027024",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580027024",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Multi-Symptom Severe Daytime",
+    "BrandCode": "81564",
+    "DrugID": "d05778",
+    "SampleNDC": "50580025808",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580025808",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold No Drowsiness",
+    "BrandCode": "5439",
+    "DrugID": "d03342",
+    "SampleNDC": "00045017224",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "00045017224",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Non-Drowsy Caplets",
+    "BrandCode": "72447",
+    "DrugID": "d03342",
+    "SampleNDC": "50580045924",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580045924",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Non-Drowsy Gelcaps",
+    "BrandCode": "72470",
+    "DrugID": "d03342",
+    "SampleNDC": "50580061620",
+    "Category": "upper respiratory combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580061620",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Relief Caplet",
+    "BrandCode": "121896",
+    "DrugID": "d03445",
+    "SampleNDC": "50580023324",
+    "Category": "analgesic combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580023324",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Relief Nighttime",
+    "BrandCode": "121955",
+    "DrugID": "d03445",
+    "SampleNDC": "50580025208",
+    "Category": "analgesic combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580025208",
+    "Specificity": "BRAND"
+  }, {
+    "DrugName": "Tylenol Cold Relief Nighttime Caplet",
+    "BrandCode": "121899",
+    "DrugID": "d03445",
+    "SampleNDC": "50580023424",
+    "Category": "analgesic combinations",
+    "CSA": {
+      "min": "0",
+      "max": "0"
+    },
+    "GenericID": "50580023424",
+    "Specificity": "BRAND"
+  }]
+}
+;
+
