c6a6649009a369c128abb4ec4b95311ad68982ee
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-07-26 23:33:23 -0400

    fixes

--- elixired/src/components/Formulary/AlternateMedication.js
+++ elixired/src/components/Formulary/AlternateMedication.js
@@ -4,18 +4,33 @@ import { GenericDrug } from '../../lib/FormularyDrug';
 
 const AlternateMedication = ({ medication }) => {
   const med = new GenericDrug(medication);
-  console.log('med', med);
 
   return (
     <tr>
-      <td>{med.getName()}</td>
-      <td>{med.getStrength()}</td>
-      <td>{med.getDoseForm()}</td>
-      <td>{med.getRoute()}</td>
-      <td>{med.getBrandType()}</td>
-      <td>{med.getPrescriptionType()}</td>
-      <td>{med.getCopayAmount()}</td>
-      <td>{med.getFormularyStatus()}</td>
+      <td>
+        {med.getName()}
+      </td>
+      <td>
+        {med.getStrength()}
+      </td>
+      <td>
+        {med.getDoseForm()}
+      </td>
+      <td>
+        {med.getRoute()}
+      </td>
+      <td>
+        {med.getBrandType()}
+      </td>
+      <td>
+        {med.getPrescriptionType()}
+      </td>
+      <td>
+        {med.getCopayAmount()}
+      </td>
+      <td>
+        {med.getFormularyStatus()}
+      </td>
     </tr>
   );
 };
---
+++ elixired/test/components/Formulary/AlternateMedication.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import expect from 'expect';
+import { mount, shallow } from 'enzyme';
+
+import AlternateMedication from '../../../src/components/Shared/AlternateMedication';
+
+describe('<AlternateMedication /> Component', () => {
+  const wrapper = shallow(<AlternateMedication />);
+});
