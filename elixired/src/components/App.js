f23b52e2529b939a9b286232c61c436c663a06d3
Gilbert F. Sewovoe-Ekoue <gsewovoe-ekoue@teladoc.com>
2017-07-31 14:52:25 -0400

    Fix

--- elixired/src/components/App.js
+++ elixired/src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
     return (
       <div className='wrapper'>
-        {/* <ClinicalMedication {...props} /> */}
+        <ClinicalMedication {...props} />
         <br />
         <br />
         {/* <ClinicalMedicationForm /> */}
