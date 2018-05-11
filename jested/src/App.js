b5e4e86b9ee14bee84519a8197ac28dbd0be70d4
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2018-05-11 08:02:05 -0400

    Jest Kata

---
+++ jested/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css';
+import Form from './Form';
+
+export default class App extends Component {
+  state = {
+    on: false,
+    input: '',
+    mainColor: 'blue',
+    lifeCycle: ''
+  }
+
+  componentDidMount() {
+    this.setState({ lifeCycle: 'componentDidMount' });
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ lifeCycle: 'componentWillReceiveProps' });
+  }
+
+  handleStrings = (str) => {
+    if (str === 'Hello World') {
+      return true;
+    }
+    return false;
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+
+          <h3 className={this.state.mainColor}>Everyone is welcome</h3>
+        </header>
+
+        <Form />
+
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className='button-state'>
+          {this.state.on ? 'Yes!' : 'No!'}
+        </p>
+
+        <button onClick={() => this.setState({ on: true })}>
+          Click
+        </button>
+
+        <h2>{this.state.input}</h2>
+        <input onChange={(e) => this.setState({input: e.currentTarget.value})} />
+        <p className='lifeCycle'>{this.state.lifeCycle}</p>
+      </div>
+    )
+  }
+}
+
+export class Link extends Component {
+  render () {
+    return this.props.hide ? null : <a href={this.props.address}>Clik</a>
+  }
+}
---
+++ jested/src/Form.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './form.css';
+import api from './api';
+
+class Form extends Component {
+  state = {
+    name: '',
+    email: '',
+    number: '',
+    optIn: true
+  };
+
+  handleChange = str => e => {
+    this.setState({ [str]: e.currentTarget.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    return api.addUser(
+      this.state.name, this.state.email, this.state.number
+    )
+  }
+
+  handlePromotionClick = e => {
+    this.setState(prevState => ({ optIn: !prevState.optIn }));
+  }
+
+  render() {
+    return (
+      <form data-testid='addUserForm' onSubmit={this.handleSubmit}>
+        <h2>Request Information</h2>
+        <input data-testid='name'
+          type='text' onChange={this.handleChange('name')}
+          placeholder='Name'
+          value={this.state.name} />
+
+        <input data-testid='email'
+          type='text' onChange={this.handleChange('email')}
+          placeholder='Email'
+          value={this.state.email} />
+
+        <input data-testid='number'
+          type='text' onChange={this.handleChange('number')}
+          placeholder='Number'
+          value={this.state.number} />
+        <div>
+          <input data-testid='checked'
+            type='checkbox'
+            checked={this.state.optIn}
+            onChange={this.handlePromotionClick} />
+          <p data-testid='promotionsP' className='promotions'>
+            Receive Promotions
+          </p>
+        </div>
+        <button type='submit'
+          data-testid='submitButton'>Submit</button>
+      </form>
+    );
+  }
+}
+
+Form.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+export default Form;
