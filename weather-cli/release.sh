3130bf430d02df713e065b8235af4d8156c154f5
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2018-07-28 20:09:56 -0400

    Add Release script

---
+++ weather-cli/release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+npm config set sign-git-tag true
+npm version $1
+
+VERSION_TAG=`node -p "require('./package.json').version"`
+git tag -s $VERSION_TAG
+# git push origin $VERSION_TAG
+
+# npm publish
+
+echo "Released $VERSION_TAG"
