b077ed933bad1dcd31670201da32e6c3fd6887a3
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2019-05-23 15:31:37 -0400

    Remove unecessary fluf

--- loan-app/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import Message from "./components/Message";
-
-ReactDOM.render(
-  <Message from='World' body='Hello !' />,
-  document.getElementById("root")
-);
